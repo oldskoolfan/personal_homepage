26c3dd90d6a746dbde44c2e63c3b5baeb88d0891
Andrew Harris <harris.1305@gmail.com>
2016-06-05 11:30:05 -0400

    added games section

---
+++ games/index.php
@@ -0,0 +1,9 @@
+<?php include "{$_SERVER['DOCUMENT_ROOT']}/include/header.php" ?>
+<h1>Games! - Very much a work in progress</h1>
+
+<nav class="vert">
+	<div>Selection</div>
+	<div><a href="/games/tanks">Tanks</a></div>
+</nav>
+
+<?php include "{$_SERVER['DOCUMENT_ROOT']}/include/footer.php" ?>
--- index.php
+++ index.php
@@ -5,5 +5,6 @@
   <div><a href="/about">About</a></div>
   <div><a href="/csci2412">CSCI 2412</a></div>
   <div><a href="/music">Music</a></div>
+  <div><a href="/games">Games</a></div>
 </nav>
 <?php include "{$_SERVER['DOCUMENT_ROOT']}/include/footer.php" ?>
