6e96940e527113870f8cdaf6df5b7b6ef0674677
Andrew Harris <harris.1305@gmail.com>
2016-10-10 22:44:31 -0400

    for now, removing games page

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <div><a href="/about">About</a></div>
   <div><a href="/csci2412">CSCI 2412</a></div>
   <div><a href="/music">Music</a></div>
-  <div><a href="/games">Games</a></div>
+  <!-- for now, don't show games link -->
+  <!--<div><a href="/games">Games</a></div>-->
 </nav>
 <p id="email-link"><a href="mailto:me@andrewfharris.com"><i class="fa fa-lg fa-envelope"></i>Email Me</a></p>
 <?php include "{$_SERVER['DOCUMENT_ROOT']}/include/footer.php" ?>
