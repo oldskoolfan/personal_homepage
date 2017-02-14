7ccd7b7b58cfdd375ad3cf70a4c19faa8162f899
Andrew Harris <harris.1305@gmail.com>
2017-02-14 10:45:12 -0500

    style tweaks

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include "{$_SERVER['DOCUMENT_ROOT']}/include/header.php" ?>
 <h1 class="hidden">Home</h1>
 <?php include 'include/nav.php' ?>
-<p>Welcome to the homepage of Andrew F Harris</p>
+<section id="welcome-msg">
+	<p>Welcome to the homepage of Andrew F Harris</p>
+</section>
 <p id="email-link"><a href="mailto:me@andrewfharris.com"><i class="fa fa-lg fa-envelope"></i>Email Me</a></p>
+
 <?php include "{$_SERVER['DOCUMENT_ROOT']}/include/footer.php" ?>
